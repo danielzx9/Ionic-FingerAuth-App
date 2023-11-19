a641eb89507c30d114aa453f6ddb822f99b590ff
DanielR <danieleduardo.rm@hotmail.com>
2023-11-19 18:08:48 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dajo4.dajo4',
+  appName: 'dajo4',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
